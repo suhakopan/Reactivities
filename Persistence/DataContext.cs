933cca674e1217d507826386492aa6d0668dede6
Suha Kopan <suha.kopan@huawei.com>
2020-10-16 09:56:54 +0300

    Initial Commit

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        
+    }
+}
