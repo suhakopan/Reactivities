b482fdc5936a56232720537b6a2b1af937f4925d
Suha Kopan <suha.kopan@huawei.com>
2020-10-21 15:04:26 +0300

    Added some good styles

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -12,12 +12,23 @@ class ActivityStore {
     @observable submitting = false;
     @observable target = '';
 
-    @computed get activitiesByDate() {
-        return Array.from(this.activityRegistry.values()).slice().sort((a, b) => Date.parse(a.date) - Date.parse(b.date))
-    }
-
     constructor() {
         makeObservable(this);
+    }
+
+    @computed get activitiesByDate() {
+        return this.groupActivitiesByDate(Array.from(this.activityRegistry.values()));
+    }
+
+    groupActivitiesByDate (activities: IActivity[]) {
+        const sortedActivities = activities.slice().sort(
+            (a, b) => Date.parse(a.date) - Date.parse(b.date)
+        )
+        return Object.entries(sortedActivities.reduce((activities, activity) => {
+            const date = activity.date.split('T')[0];
+            activities[date] = activities[date] ? [...activities[date], activity] : [activity]
+            return activities;
+        }, {} as {[key: string]: IActivity[]}));
     }
 
     @action loadActivities = async () => {
